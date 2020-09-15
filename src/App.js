a0a6cc640f3cdb8aed681a544b25ab2a8e35e193
Mariam_Naz <mariamnaz212@gmail.com>
2020-09-15 18:26:25 +0500

    Complete form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./stylesheets/style.css";
-import { Switch, Route } from "react-router-dom";
 import Cover from "./Components/Cover";
 import Form from "./Components/Form";
 
 export default function App() {
   return (
     <>
-      <Switch>
-        <Route exact path="/" Component={Form} />
-        <Route exact path="/react.org" Component={Cover} />
-      </Switch>
+      <Form />
     </>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,22 +1,55 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = (event) => {
+    console.log(this.State);
+  };
+
   render() {
     return (
       <>
-        <form>
-          <div className="mb-3">
-            <label className="form-label">Email address</label>
-            <input type="email" className="form-control" />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input type="password" className="form-control" />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        <div className="col-6 mx-auto my-5">
+          <h1 className="text-center display-4">Login</h1>
+          <form className="mt-5">
+            <div className="mb-3">
+              <label className="form-label">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                onChange={(e) => {
+                  this.setState({ email: e.target.value });
+                }}
+                value={this.state.email}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                onChange={(e) => {
+                  this.setState({ password: e.target.value });
+                }}
+                value={this.state.password}
+              />
+            </div>
+            <button
+              onClick={this.handleSubmit}
+              type="submit"
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </>
     );
   }
